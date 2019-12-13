228fb696c77c891cc5a1b0dd3ae61dbb1ddd88d9
ghkdtlsvlf <tlsvlfghkds@naver.com>
2019-12-13 22:43:29 +0900

    air

--- To_the_Light/To_the_Light/aircraft.cpp
+++ To_the_Light/To_the_Light/aircraft.cpp
@@ -46,15 +46,20 @@ void CAircraft::handle_event(Event a_event, int mouse_x, int mouse_y) {
 		pos = pos + direction;
 		break;
 	case A_KEY_DOWN:
+		glm::vec3 left = glm::vec3{ glm::cos(glm::radians(angle - 90)) , 0 ,  glm::sin(glm::radians(angle - 90)) } *0.2f;
+		pos = pos + left;
 		break;
 	case S_KEY_DOWN:
-
+		pos = pos - direction;
 		break;
 	case D_KEY_DOWN:
-		break
-
-
-
+		glm::vec3 right = glm::vec3{ glm::cos(glm::radians(angle + 90)) , 0 ,  glm::sin(glm::radians(angle + 90)) } *0.2f;
+		pos = pos + right;
+		break;
+	case SPACE_KEY_UP:
+		glm::vec3 up = glm::vec3{ 0 , glm::cos(glm::radians(angle + 90)) ,  glm::sin(glm::radians(angle + 90)) } *0.2f;
+		pos = pos + up;
+		break;
 	}
 
 
--- To_the_Light/To_the_Light/aircraft.h
+++ To_the_Light/To_the_Light/aircraft.h
@@ -9,13 +9,14 @@ class CAircraft: public CGameObject
 	float view_y;
 
 	glm::vec3 pos = { 0,0,0 };
-	glm::vec3 direction;
+	glm::vec3 direction = glm::vec3{ glm::cos(glm::radians(angle)) , 0 ,  glm::sin(glm::radians(angle)) } *0.2f;
+	glm::vec3 height;
 	float angle = 270;
 
 	glm::mat4 transform = glm::mat4{ 1.0, };
 	glm::mat4 rotate_world = glm::rotate(rotate_world, glm::radians(-angle), glm::vec3{ 0,1,0 });
 	glm::mat4 translate_world = glm::translate(translate_world, pos);
-
+	
 
 	GLuint vao;
 	GLuint vbo[2];
