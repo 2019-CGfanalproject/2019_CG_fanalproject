8d2b8ac341e6425c339ffda672df628c15f7c22a
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-12-14 21:05:21 +0900

    타이틀 띄우는 중

--- To_the_Light/To_the_Light/titlescene.cpp
+++ To_the_Light/To_the_Light/titlescene.cpp
@@ -1,5 +1,5 @@
 #include "titlescene.h"
-#include "pch.h"
+#include "shader.h"
 #include "framework.h"
 
 CTitleScene::CTitleScene()
@@ -16,10 +16,37 @@ void CTitleScene::initalize(CFramework* p_fw)
 	std::cout << "Title" << std::endl;
 	m_framework = p_fw;
 
+	shader_id = *m_framework->get_shader_id();
+	model_location = glGetUniformLocation(shader_id, "modelTransform");
+	veiw_location = glGetUniformLocation(shader_id, "viewTransform");
+	projection_location = glGetUniformLocation(shader_id, "projectionTransform");
+	object_color_location = glGetUniformLocation(shader_id, "object_color");
+
+
+	loadObj("Resource/Object/Title.obj", title_vertex, title_normal, title_uv);
+
+	glGenVertexArrays(1, &title_vao);
+	glGenBuffers(2, title_vbo);
+
+	glBindVertexArray(title_vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, title_vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, title_vertex.size() * sizeof(glm::vec3), &title_vertex[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, title_vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, title_normal.size() * sizeof(glm::vec3), &title_normal[0], GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), 0);
+	glEnableVertexAttribArray(1);
 }
 
 void CTitleScene::draw()
 {
+	glBindVertexArray(title_vao);
+	glUniformMatrix4fv(model_location, 1, GL_FALSE, value_ptr(transform));
+	glUniform3f(object_color_location, 1, 1, 0);
+	glDrawArrays(GL_TRIANGLES, 0, title_vertex.size());
 }
 
 void CTitleScene::update(std::chrono::milliseconds framtime)
--- To_the_Light/To_the_Light/titlescene.h
+++ To_the_Light/To_the_Light/titlescene.h
@@ -1,10 +1,26 @@
 #pragma once
 #include "scene.h"
+#include "pch.h"
+
+using namespace glm;
 
 class CFramework;
 class CTitleScene : public CScene
 {
 	CFramework* m_framework{ nullptr };
+
+	GLuint shader_id;
+
+	GLuint model_location;
+	GLuint veiw_location;
+	GLuint projection_location;
+	GLuint object_color_location;
+
+	mat4 transform = mat4(1);
+
+	GLuint title_vao, title_vbo[2];
+	std::vector< glm::vec3 > title_vertex, title_normal;
+	std::vector< glm::vec2 > title_uv;
 
 	int m_mouse_x;
 	int m_mouse_y;
